406af3875f76c10e9e86cdee6bd9d6e1c174fe74
Ganpat Suthar <rsutharganpat2004@gmail.com>
2026-02-06 05:20:32 +0530

    added supabase

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine # type: ignore
 from sqlalchemy.orm import sessionmaker # type: ignore
 from ..core.config import settings
 
-engine  = create_engine(settings.DATABASE_URL , future =True)
+engine  = create_engine(settings.DATABASE_URL , future =True, connect_args={"sslmode": "require"}, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit = False , autoflush = False , bind = engine)
 
 def get_db():
